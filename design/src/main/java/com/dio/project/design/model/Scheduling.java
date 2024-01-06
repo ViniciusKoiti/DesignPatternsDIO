81961d075515b5e41d061b6e2f5857a0178b5e6d
Vinicius Koiti <viniciusnakahara@gmail.com>
2024-01-06 11:48:47 -0300

    Corrigindo entity

--- design/src/main/java/com/dio/project/design/model/Scheduling.java
+++ design/src/main/java/com/dio/project/design/model/Scheduling.java
@@ -1,4 +1,19 @@
 package com.dio.project.design.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
 public class Scheduling {
+    @Id
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
