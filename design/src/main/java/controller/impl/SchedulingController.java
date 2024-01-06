24130c1e18a9637b29f15c6f80fbfbf2f1e625c0
Vinicius Koiti <viniciusnakahara@gmail.com>
2024-01-06 00:37:02 -0300

    Melhorando testes e configurando Mock

--- design/src/main/java/controller/impl/SchedulingController.java
+++ design/src/main/java/controller/impl/SchedulingController.java
@@ -5,10 +5,7 @@ import dto.SchedulingDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import service.SchedulingService;
 
 @RestController
--- design/src/main/java/service/CrudService.java
+++ design/src/main/java/service/CrudService.java
@@ -3,12 +3,13 @@ package service;
 import common.constant.PageConstant;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
 public interface CrudService<T> {
 
     ResponseEntity<T> create(T objetoVo);
-    ResponseEntity<T> getById(long id);
+    ResponseEntity<T> getById(@PathVariable long id);
 
     ResponseEntity<Page<T>> getItems(int page,
                                      int size);
